5ea6139701fc5ce7b72bb2cefcd174f0954c35ac
Nusrath <Nusrathanif234@gmail.com>
2026-04-13 22:44:26 +0100

    Update home screen to use HomeScreen widget instead of MyHomePage

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: const Color.fromARGB(255, 1, 44, 15)),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen()
     );
   }
 }
