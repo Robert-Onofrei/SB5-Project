14282dc48e67bcd2497b1d709679e27d7bbce2ab
lough <lough@DESKTOP-4RUONDR>
2026-04-28 17:48:23 +0100

    Fixed reason for kit build failure

--- frontend/lib/trail_map_screen.dart
+++ frontend/lib/trail_map_screen.dart
@@ -54,7 +54,7 @@ class TrailMapScreen extends StatelessWidget {
                         borderRadius: BorderRadius.circular(4),
                         boxShadow: [
                           BoxShadow(
-                            color: Colors.black.withOpacity(0.2),
+                            color: Colors.black.withValues(alpha: 0.2),
                             blurRadius: 4,
                           ),
                         ],
